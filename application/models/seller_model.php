eea284bc804cacd2e3e8ba6658974b85cd39f05d
Pankaj Tiwari <pankajtiwarisagar@gmail.com>
2015-01-17 04:16:46 +0530

    coded Seller with tag functionality

--- application/models/seller_model.php
+++ application/models/seller_model.php
@@ -1,10 +1,18 @@
 <?php
 
+class SellerTags {
+
+    public static $deleted = 0;
+    public static $available = 1;
+
+}
+
 class Seller_model extends CI_Model {
 
 var $seller_name = "";
 var $seller_phone_number = "";
 var $seller_address = "";
+var $tag = NULL;
 
     public function __construct()
 	{
@@ -15,6 +23,7 @@ var $seller_address = "";
         $this->seller_name = $name;
         $this->seller_phone_number = $phone;
         $this->seller_address = $address;
+        $this->tag = SellerTags::$available;
 
         $this->db->insert("seller", $this);
     }
@@ -23,29 +32,33 @@ var $seller_address = "";
         $this->seller_name = $name;
         $this->seller_phone_number = $phone;
         $this->seller_address = $address;
+        $this->tag = SellerTags::$available;
 
         $this->db->update('seller', $this, array('id' => $id));
 
     }
     
     function delete($id){
+        
         $this->db->where('id', $id);
-        $this->db->delete('seller'); 
+        $this->db->update('seller',array('tag'=>  SellerTags::$deleted)); 
     }
     
     function get_all_entries(){
-         $query = $this->db->get('seller');
+         $query = $this->db->get_where('seller',array('tag'=>  SellerTags::$available));
          return $query->result();
     }
     function get_one_seller($id){
         
-        $query = $this->db->get_where('seller',array('id'=>$id));
+        $query = $this->db->get_where('seller',array('id'=>$id,'tag'=> SellerTags::$available));
         return $query->result();
     }
     function get_all_other_entries($product_id){
-        $query = $this->db->query("SELECT * FROM seller WHERE id not in (select seller_id as id from product_seller_mapping where product_id=$product_id);");
+        $query = $this->db->query("SELECT * FROM seller WHERE id not in (select seller_id as id from product_seller_mapping where product_id=$product_id)and tag=".SellerTags::$available.";");
         return $query->result();
     }
     
 
 }
+
+
