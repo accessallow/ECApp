5e5c061fe4afde9c4d42bea9e22834a500c277cb
Pankaj Tiwari <pankajtiwarisagar@gmail.com>
2015-01-17 04:38:58 +0530

    coded Product category with tag functionality

--- application/models/product_category_model.php
+++ application/models/product_category_model.php
@@ -1,9 +1,16 @@
 <?php
- 
+
+class ProductCategoryTags {
+
+    public static $deleted = 0;
+    public static $available = 1;
+
+}
+
 class Product_category_model extends CI_Model {
 
-    
     var $product_category_name = "";
+    var $tag = NULL;
 
     public function __construct() {
         $this->load->database();
@@ -11,32 +18,35 @@ class Product_category_model extends CI_Model {
 
     function insert($name) {
         $this->product_category_name = $name;
+        $this->tag = ProductCategoryTags::$available;
 
         $this->db->insert("product_category", $this);
     }
 
     function edit($id, $name) {
         $this->product_category_name = $name;
-
+        $this->tag = ProductCategoryTags::$available;
+        
         $this->db->update("product_category", $this, array('id' => $id));
     }
 
     function delete($id) {
         $this->db->where('id', $id);
-        $this->db->delete("product_category");
+        $this->db->update("product_category",array('tag'=>  ProductCategoryTags::$deleted));
     }
 
     function get_all_entries() {
-        $query = $this->db->get("product_category");
+        $query = $this->db->get_where("product_category",array('tag'=>  ProductCategoryTags::$available));
         return $query->result();
     }
 
     function get_one_category($id) {
 
-        $query = $this->db->get_where("product_category", array('id' => $id));
+        $query = $this->db->get_where("product_category", array('id' => $id,'tag'=>  ProductCategoryTags::$available));
         return $query->result();
     }
-    function get_category_name($id){
+
+    function get_category_name($id) {
         $a = $this->get_one_category($id);
         return $a[0]->product_category_name;
     }
