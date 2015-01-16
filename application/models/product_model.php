35090ced5ace3c0e839adc2ea37c2d1979deed11
Pankaj Tiwari <pankajtiwarisagar@gmail.com>
2015-01-17 04:02:04 +0530

    Product coded with tag functionality

--- application/models/product_model.php
+++ application/models/product_model.php
@@ -1,11 +1,20 @@
 <?php
+
 //done 
+class ProductTags {
+
+    public static $deleted = 0;
+    public static $available = 1;
+
+}
+
 class Product_model extends CI_Model {
 
     var $product_name = "";
     var $product_brand = "";
     var $product_category = "";
     var $product_description = "";
+    var $tag = NULL;
 
     public function __construct() {
         $this->load->database();
@@ -16,36 +25,42 @@ class Product_model extends CI_Model {
         $this->product_brand = $brand;
         $this->product_category = $category;
         $this->product_description = $description;
+        $this->tag = ProductTags::$available;
+        
         $this->db->insert("products", $this);
     }
 
-    function edit($id,$name, $brand, $category, $description) {
+    function edit($id, $name, $brand, $category, $description) {
         $this->product_name = $name;
         $this->product_brand = $brand;
         $this->product_category = $category;
         $this->product_description = $description;
+        $this->tag = ProductTags::$available;
 
         $this->db->update('products', $this, array('id' => $id));
     }
 
     function delete($id) {
+
+        $data = array('tag' => ProductTags::$deleted);
         $this->db->where('id', $id);
-        $this->db->delete('products');
+        $this->db->update('products', $data);
     }
 
     function get_all_entries() {
-        $query = $this->db->get('products');
+        $query = $this->db->get_where('products',array('tag'=>  ProductTags::$available));
         return $query->result();
     }
-    function get_all_entries_joined(){
+
+    function get_all_entries_joined() {
         $query = $this->db->query('SELECT p.id,p.product_name,p.product_brand,c.product_category_name as \'product_category\','
-                . 'p.product_description FROM products p,product_category c WHERE p.product_category = c.id;');
+                . 'p.product_description FROM products p,product_category c WHERE p.product_category = c.id and p.tag = '.ProductTags::$available.';');
         return $query->result();
     }
 
     function get_one_product($id) {
 
-        $query = $this->db->get_where('products', array('id' => $id));
+        $query = $this->db->get_where('products', array('id' => $id,'tag'=>  ProductTags::$available));
         return $query->result();
     }
 
